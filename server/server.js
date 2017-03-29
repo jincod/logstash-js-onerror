aaa9369cee4dabd54b6efa34b20a46eafb2d87c9
jincod <vadim.abdrashitov@gmail.com>
2017-03-29 14:41:57 +0500

    updated API

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 const winston = require('winston');
-var bodyParser = require('body-parser');
-
 const logger = require('./logger');
+const config = require('./config.json');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const getLevel = (level = 'error') => {
   if (Object.keys(winston.config.npm.levels).indexOf(level) !== -1) {
@@ -17,9 +19,12 @@ const getLevel = (level = 'error') => {
 }
 
 app.post('/api/log', (req, res) => {
-  const {projectid, appid, message, level} = req.body;
+  const {message, meta = {}} = req.body;
+  const {projectid, appid, level} = meta;
 
-  logger[getLevel(level)](message, {projectid, appid});
+  if (appid && projectid && config.projects[projectid]) {
+    logger[getLevel(level)](message, meta);
+  }
 
   res.sendStatus(200);
 });
--- src/index.js
+++ src/index.js
@@ -4,30 +4,40 @@ export default {
     if(!options.url) {
       throw 'url not setted';
     }
-    if(!options.projectName) {
-      throw 'projectName not setted';
+    if(!options.appid) {
+      throw 'appid not setted';
     }
-    if(!options.projectId) {
-      throw 'projectId not setted';
+    if(!options.projectid) {
+      throw 'projectid not setted';
     }
 
     this.options = options;
 
     window.onerror = (msg, url, line, column, errorObj) =>
-      this.log({url, msg: `${msg} ${line} ${column} ${JSON.stringify(errorObj)}`});
+      this.log(msg, {
+        url,
+        line,
+        column,
+        level: 'error',
+        stack: JSON.stringify(errorObj)
+      });
   },
 
-  extendData(data) {
-    const level = data.level || 'ERROR';
-
-    return `${new Date().toISOString()}|${this.options.projectName}|
-      ${this.options.projectId}|0|${level}|${data.url}|${data.msg}`;
+  extendData(meta) {
+    return Object.assign({
+      projectid: this.options.projectid,
+      appid: this.options.appid
+    }, meta);
   },
 
-  log(data) {
-    fetch(this.options.url, {
-      method: 'post',
-      body: this.extendData(data)
-    });
+  log(message, meta) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('post', this.options.url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.send(JSON.stringify({
+      message,
+      meta: this.extendData(meta)
+    }));
   }
 }
